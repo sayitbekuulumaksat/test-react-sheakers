b22091a161ccded85f522907194c313c5f0cb258
Saiitbekuulu Maksatbek <sayitbekuulumaksat@gmail.com>
2025-02-11 23:04:56 +0600

    removed basename for BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/test-react-sheakers/">
+  <BrowserRouter >
     <StrictMode>
       <Provider store={store}>
         <App />
